57338b6a986d2bfab42addddd4355c3b809636bd
Cristhian Cordova <crithbray@gmail.com>
2023-09-05 20:08:08 -0500

    Tercer commit - param enable, incluyendo y excluyendo metodos

--- src/test/java/anotacionestestng/AnotacionesBasicasTest.java
+++ src/test/java/anotacionestestng/AnotacionesBasicasTest.java
@@ -6,25 +6,21 @@ public class AnotacionesBasicasTest {
 
     @BeforeClass
     public void codigoAntes(){
-
         System.out.println("Este codigo se ejecuta antes de la clase --/");
     }
 
     @AfterClass
     public void codigoDespues(){
-
         System.out.println("Este codigo se ejecuta despues de la clase --/");
     }
 
     @BeforeSuite
     public void codigoAntesSuite(){
-
         System.out.println("Este codigo se ejecuta antes de toda la suite --*");
     }
 
     @AfterSuite
     public void codigoDespuesSuite(){
-
         System.out.println("Este codigo se ejecuta despues de toda la suite--*");
     }
 
@@ -50,14 +46,11 @@ public class AnotacionesBasicasTest {
 
     @Test
     public void pruebaUno(){
-
         System.out.println("Esta es la prueba uno");
     }
 
     @Test(dependsOnMethods = {"pruebaUno"})
     public void pruebaDos(){
-
         System.out.println("Esta es la prueba dos");
     }
-
 }
--- src/test/java/anotacionestestng/SegundoSetTest.java
+++ src/test/java/anotacionestestng/SegundoSetTest.java
@@ -5,13 +5,31 @@ import org.testng.annotations.Test;
 public class SegundoSetTest {
     @Test
     public void pruebaTres(){
-
         System.out.println("Esta es la prueba tres del segundo set");
     }
 
     @Test(dependsOnMethods = {"pruebaTres"})
     public void pruebaCuatro(){
-
         System.out.println("Esta es la prueba cuatro del segundo set");
     }
+
+    @Test(enabled = true)
+    public void pruebaCinco(){
+        System.out.println("Esta es la prueba cinco del segundo set");
+    }
+    @Test
+    public void clicAlHomePage() {
+        System.out.println("Clic al Home");
+    }
+
+    @Test
+    public void clicAlCarrito() {
+        System.out.println("clic al carrito");
+    }
+
+    @Test
+    public void clicAlUsar() {
+        System.out.println("clic al usuario");
+    }
+
 }
